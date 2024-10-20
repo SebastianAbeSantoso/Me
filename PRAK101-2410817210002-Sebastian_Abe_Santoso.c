70f28a6afa7260771da05fed9f667149c5fe7d42
SebastianAbeSantoso <sebastian.abe.santoso@gmail.com>
2024-10-20 14:35:03 +0800

    Add files via upload

---
+++ PRAK101-2410817210002-Sebastian_Abe_Santoso.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    int  x = 5, y = 7, z = 9, total = x + y + z;
+    printf("Variabel x bernilai %d\nVariabel y bernilai %d\nVariabel z bernilai %d\nJumlah variabel tersebut adalah %d", x, y, z, total);
+    return 0;
+}
