9e41bfcdb8cd7d0de8be0669646fadf904725de7
Sebastian Abe Santoso <sebastian.abe.santoso@gmail.com>
2026-03-12 10:57:08 +0800

    types

---
+++ LAPORAN_PEMROGRAMAN_MOBILE/week 4/exercise_5/apps/web/modules/types.ts
@@ -0,0 +1,26 @@
+export type Restaurant = {
+  id: string
+  name: string
+  description: string
+  categories: string[]
+  logoUrl: string
+  heroImageUrl: string
+  deliveryFee: number
+  deliveryTime: [number, number]
+  rating: number
+  numRatings: number
+  reviews: CustomerReview[]
+}
+
+export type CustomerReview = {
+  rating: number
+  comment: string
+  date: string
+  userName: string
+}
+
+export type Ratings = {
+  rating: number
+  numRatings: number
+  reviews: CustomerReview[]
+}
