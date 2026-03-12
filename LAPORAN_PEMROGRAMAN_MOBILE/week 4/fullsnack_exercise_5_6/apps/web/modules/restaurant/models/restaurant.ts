ba6edaf6ef94577331420d798737b9f9a2adc540
Sebastian Abe Santoso <2410817210002@mhs.ulm.ac.id>
2026-03-12 10:58:52 +0800

    yeye

--- LAPORAN_PEMROGRAMAN_MOBILE/week 4/fullsnack_exercise_5_6/apps/web/modules/restaurant/models/restaurant.ts
+++ LAPORAN_PEMROGRAMAN_MOBILE/week 4/fullsnack_exercise_5_6/apps/web/modules/restaurant/models/restaurant.ts
@@ -8,7 +8,20 @@ import { Ratings, Restaurant } from './types'
 
 export async function getRestaurant(id: string): Promise<Restaurant> {
   const restaurant = await fetchRestaurant(id)
-@@ -20,3 +25,20 @@ export async function getRestaurant(id: string): Promise<Restaurant> {
+  const { attributes, relationships } = restaurant
+
+  const categories = await fetchRestaurantCategories(relationships.categories)
+  const reviews = relationships.reviews.map((review) => ({
+    rating: review.rating,
+    comment: review.comment,
+    date: review.date,
+    userName: review.user.name,
+  }))
+
+  return {
+    id: restaurant.id,
+    ...attributes,
+    categories: categories.map((c) => c.name),
     reviews,
   }
 }
